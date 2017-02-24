56107dee0e7933a340aa3508f4bb752d4362fe3e
Robert Wagner <rwwagner90@gmail.com>
2017-02-24 08:03:17 -0500

    Use node assets instead of node modules to vendor

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 /* jshint node: true */
 'use strict';
 
-const transformAMD = (name) => ({
-  using: [{ transformation: 'amd', as: name }]
-});
-
 module.exports = {
   name: 'ember-redux-actions',
 
   options: {
-    nodeModulesToVendor: [
-      'node_modules/redux-actions/dist'
-    ]
-  },
-
-  included() {
-    this._super.included.apply(this, arguments);
-
-    this.import('vendor/redux-actions.js', transformAMD('redux-actions'));
+    nodeAssets: {
+      'redux-actions': {
+        import: [{
+          path: 'dist/redux-actions.js',
+          using: [{ transformation: 'amd', as: 'redux-actions' }]
+        }]
+      }
+    }
   }
 };
